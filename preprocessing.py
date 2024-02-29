82786e72157bf15cc7d5daec25f9df1283b2db9d
Denny <ciccia.denny@gmail.com>
2024-02-29 11:20:19 +0100

    reso quiet il download delle librerie nltk

--- preprocessing.py
+++ preprocessing.py
@@ -5,10 +5,10 @@ import re
 STEMMING = True
 LEMMATIZATION = not STEMMING
 
-nltk.download("punkt")
-nltk.download("stopwords")
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
+nltk.download("punkt", quiet=True)
+nltk.download("stopwords", quiet=True)
+nltk.download('averaged_perceptron_tagger', quiet=True)
+nltk.download('wordnet', quiet=True)
 
 stop_words = set(nltk.corpus.stopwords.words('english'))
 stemmer = nltk.SnowballStemmer("english")

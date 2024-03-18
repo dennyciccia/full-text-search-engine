a4837d0c17adf3e238a5680d5556d71e71ef59cc
Denny <ciccia.denny@gmail.com>
2024-03-18 14:16:05 +0100

    iniziato benchmark

---
+++ benchmark/benchmark_script.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from src.InvertedIndex import InvertedIndex
+
+UIN_PATH = "UIN.csv"
+INDEX_DIR = "../data/index"
+
+def do_benchmark():
+    # apertura inverted index
+    index = InvertedIndex(INDEX_DIR)
+    index.open_index()
+
+    # lettura delle query dal file
+    # queries è una lista di tuple, ogni tupla ha la query in forma di stringa e una lista di id dei documenti rilevanti
+    df = pd.read_csv(UIN_PATH, header=0, sep=';')
+    queries = []
+    for _, row in df.iterrows():
+        queries.append((row['query'], row['R']))
+
+    # array dei documenti rilevanti di tutte le query
+    Ra_tot = []
+
+    # esecuzione delle query e registrazione dei risultati
+    for q,R in queries:
+        query = dict((k.strip(), v.strip()) for k, v in (element.split(': ') for element in q.split(', ')))
+        A = index.search_documents(**query)
+
+        # documenti rilevanti trovati
+        Ra = []
+        for doc in A:
+            if doc in R:
+                Ra.append(doc)
+
+        Ra_tot.append(Ra)
+
+        # da finire ...
+
+
+
+if __name__ == "__main__":
+    do_benchmark()

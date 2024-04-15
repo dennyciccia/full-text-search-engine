10c829ef3e6b0f888a03a1b6c15e8bff257909ab
Denny <ciccia.denny@gmail.com>
2024-04-15 11:47:59 +0200

    update

--- benchmark/benchmark_script.py
+++ benchmark/benchmark_script.py
@@ -4,23 +4,25 @@ from src.InvertedIndex import InvertedIndex
 UIN_PATH = "UIN.csv"
 INDEX_DIR = "../data/index"
 
-def do_benchmark():
-    # apertura inverted index
-    index = InvertedIndex(INDEX_DIR)
-    index.open_index()
+def mean_average_precision(index, queries):
+    pass
 
-    # lettura delle query dal file
-    # queries è una lista di tuple, ogni tupla ha la query in forma di stringa e una lista di id dei documenti rilevanti
-    df = pd.read_csv(UIN_PATH, header=0, sep=';')
-    queries = []
-    for _, row in df.iterrows():
-        queries.append((row['query'], row['R']))
+def DCG(index, queries):
+    for q in queries:
+        # esecuzione query
+        query = dict((k.strip(), v.strip()) for k, v in (element.split(': ') for element in q.split(', ')))
+        results = index.search_documents(**query)
 
+        # mostra 1 risultato alla volta
+            # chiede il punteggio
+
+
+def R_qualcosa(index, queries):
     # array dei documenti rilevanti di tutte le query
     Ra_tot = []
 
     # esecuzione delle query e registrazione dei risultati
-    for q,R in queries:
+    for q, R in queries:
         query = dict((k.strip(), v.strip()) for k, v in (element.split(': ') for element in q.split(', ')))
         A = index.search_documents(**query)
 
@@ -34,6 +36,20 @@ def do_benchmark():
 
         # da finire ...
 
+def do_benchmark():
+    # apertura inverted index
+    index = InvertedIndex(INDEX_DIR)
+    index.open_index()
+
+    # lettura delle query dal file
+    # queries è una lista di tuple, ogni tupla ha la query in forma di stringa e una lista di id dei documenti rilevanti
+    df = pd.read_csv(UIN_PATH, header=0, sep=';')
+    queries = []
+    for _, row in df.iterrows():
+        queries.append((row['query'], row['R']))
+
+    DCG(index, queries)
+
 
 
 if __name__ == "__main__":
@@ -46,4 +62,4 @@ if __name__ == "__main__":
 # - E-measure
 # misure che non hanno bisogno di R:
 # - average precision e mean average precision
-# - DGC
+# - DCG
